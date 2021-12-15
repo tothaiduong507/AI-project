2201859d8c107af2d1853dc346885e1362c317ee
dnht <95595731+dnht@users.noreply.github.com>
2021-12-15 08:16:57 +0700

    Update input_AI.py

--- input_AI.py
+++ input_AI.py
@@ -12,7 +12,7 @@ for i in range(R):
 #dien cac vi tri tren ban do 
 print("Pls insert the map (ware house, live tree, dead tree, new seed, obstacle)")   
 for i in range(R):
-        rowth = input().split("  ") #nhap vao mot hang gom C doi tuong ngan cach boi "  "
+        rowth = input().split("  ") #nhap vao mot hang gom C doi tuong ngan cach boi "2-spaces"
         for j in range(C):
             matrix[i][j] = rowth[j]
 print(matrix)
