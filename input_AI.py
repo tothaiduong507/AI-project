52729441da7a25a0c4a0604be3766bc0ffedf40d
dnht <95595731+dnht@users.noreply.github.com>
2021-12-15 08:15:03 +0700

    Add files via upload

---
+++ input_AI.py
@@ -0,0 +1,18 @@
+R = int(input("Row size = ")) #rowsize
+C = int(input("Column size = ")) #columnsize
+
+#thiet lap so do 
+matrix = []
+for i in range(R):
+    rowth = []
+    for j in range(C):
+        rowth.append([j])
+    matrix.append(rowth)
+
+#dien cac vi tri tren ban do 
+print("Pls insert the map (ware house, live tree, dead tree, new seed, obstacle)")   
+for i in range(R):
+        rowth = input().split("  ") #nhap vao mot hang gom C doi tuong ngan cach boi "  "
+        for j in range(C):
+            matrix[i][j] = rowth[j]
+print(matrix)
